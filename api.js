2a6fb67e602f297dbd0faa2c47599bec54729afc
rodrigorodriguesb <rodrigo.rodrigues@sou.inteli.edu.br>
2022-05-25 18:50:01 -0300

    teste

--- api.js
+++ api.js
@@ -1,31 +1,55 @@
+
 const express = require('express'); 
-const app = express();
-
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 const hostname = '127.0.0.1';
-const port = 3051;
-const sqlite3 = require('sqlite3').verbose(); 
-const DBPATH = 'revirarDB.db'; 
-app.use(express.static("/"));
-app.use(express.json());
 
-app.get('/users', (req, res) => {
-res.statusCode = 200;
-//res.setHeader('Content-Type', 'text/html');
-res.setHeader('Access-Control-Allow-Origin', '*');
-var db = new sqlite3.Database(DBPATH);
-var sql = 'SELECT * FROM atendimentos';
-db.get(sql, [], (err, row) => {
-    if (err) {
-    throw err;
-    }
-    //res.write("<h1> Teste do banco de dados</h1>") 
-    //res.write("<h4> Informacoes do usuario cujo id = 1: </h2>") 
-    //res.write("title = " + row.title); 
-    //res.write("<br />completed = " + row.completed); 
-    res.json(row);
-});
+/* Servidor do Banco de Dados */
+const portback = 3061;
+const sqlite3 = require('sqlite3').verbose();
+const server = express();
+const DBPATH = 'revirarDB.db';
+server.use(express.static('.'));
+server.use(express.json());
+
+
+/* Definição dos endpoints */
+
+/****** CRUD ******************************************************************/
+
+// Retorna todos registros (é o R do CRUD - Read)
+server.get('/users', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = 'SELECT * FROM acolhido';
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
 });
 
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+/* Inicia o servidor */
+server.listen(portback, hostname, () => {
+  console.log(`BD server running at http://${hostname}:${portback}/`);
+});
+
+// Insere um registro (é o C do CRUD - Create)
+server.post('/userinsert', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "INSERT INTO acolhido (id_atendido) VALUES ('3')";
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+	});
+	db.close(); // Fecha o banco
+	res.end();
 });
