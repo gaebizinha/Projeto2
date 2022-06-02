f5073261120650bebaac9b0681341a9893678d1f
rodrigorodriguesb <rodrigo.rodrigues@sou.inteli.edu.br>
2022-06-02 15:03:22 -0300

    concertando

--- Index/script.js
+++ Index/script.js
@@ -3,3 +3,35 @@ window.addEventListener("scroll", function(){
     img.classList.toggle("sticky", window.scrollY > 0)  
 })  
 
+api = 'http://127.0.0.1:3061'
+
+
+$(document).ready(() => {
+    users.list();
+});
+
+
+var users = {
+    
+    list() {
+        $.ajax({
+            url: api + '/users',
+            type: 'GET',
+            success: data => {
+                var tx = '';
+                tx += '<div class="centerList">';
+                data.forEach(element => {
+                    tx += '<div class="card slim">';
+                        tx += '<h2 class="ttl2">' + element.nm_nome + '</h2>';
+                        tx += '<p class="txt1">' + element.dt_nascimento + '</p>';
+                        tx += '<div class="actions">';
+                        tx += '</div>';
+                    tx += '</div>';
+                });
+                $('#main').html(tx);
+            }
+        });
+        
+    }
+    
+};
