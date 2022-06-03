795ef82069edff1a4589ce53f048eea09b6f8746
FelipeSantosInteli <99196112+FelipeSantosInteli@users.noreply.github.com>
2022-06-03 10:56:14 -0300

    header e footer unicos e imagens atualizadas

--- Index/script.js
+++ Index/script.js
@@ -2,3 +2,8 @@ window.addEventListener("scroll", function(){
     var img = document.querySelector("img");
     img.classList.toggle("sticky", window.scrollY > 0)  
 })  
+
+$(document).ready(function(){       
+    $("#header").load("NavBar.html")
+    $("#footer").load("Footer.html")
+});
