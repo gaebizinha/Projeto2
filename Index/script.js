f6267536408bf14ab024dc5b5769adfa080a43af
rodrigorodriguesb <rodrigo.rodrigues@sou.inteli.edu.br>
2022-06-07 14:25:13 -0300

    requisição post

--- Index/script.js
+++ Index/script.js
@@ -35,3 +35,35 @@ var users = {
     }
     
 };
+var user = {
+
+    insert() {
+        var nome = document.querySelector("#nome").value;
+        var dataN = document.querySelector("#dataN").value;
+        var cpf = parseInt(document.querySelector("#cpf").value);
+        var rg = parseInt(document.querySelector("#rg").value);
+        var ds_locais = document.querySelector("#ds_locais").value;
+        var apl = document.querySelector("#apelido").value;
+
+    
+
+        if (nome) {
+            if (nome.trim() != '') {
+                $.ajax({
+                    type: 'POST',
+                    url: api + '/userinsert',
+                    data: {nm_nome: nome, dt_nas: dataN, nr_cpf: cpf, nr_rg: rg, ds_locais: ds_locais},
+                }).done(function () {
+                    users.list();
+                }).fail(function (msg) {
+                    //console.log('FAIL');
+                }).always(function (msg) {
+                    //console.log('ALWAYS');
+                });
+            }
+        }
+    },
+}
+
+
+
