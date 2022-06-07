42549f018748c3de8dbafd7a1f4690e11e71f71a
manumorais <emanuele.martins@sou.inteli.edu.br>
2022-06-07 14:30:59 -0300

    .

--- Index/script.js
+++ Index/script.js
@@ -1,4 +1,4 @@
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function(){ 
     var img = document.querySelector("img");
     img.classList.toggle("sticky", window.scrollY > 0)  
 })  
@@ -33,6 +33,32 @@ var users = {
                     tx += '</div>';
                 });
                 $('#main').html(tx);
+            }
+        });
+        
+    }
+    
+};
+
+var users = {
+    
+    list() {
+        $.ajax({
+            url: api + '/vePostagem',
+            type: 'GET',
+            success: data => {
+                var tx = '';
+                tx += '<div class="centerList">';
+                data.forEach(element => {
+                    tx += '<div class="card slim">';
+                        tx += '<h2 class="ttl2">' + element.tlt_post + '</h2>';
+                        tx += '<p class="txt1" pattern="[0-9]{2}-[0-9]{2}-[0-9]{4}">' + element.dt_post + '</p>';
+                        tx += '<p class="txt1">' + element.txt_post + '</p>';  
+                        tx += '<div class="actions">';
+                        tx += '</div>';
+                    tx += '</div>';
+                });
+                $('#blog').html(tx);
             }
         });
         
