d39022f7f43086927fce63b087a2ceb4bd29a4d9
manumorais <emanuele.martins@sou.inteli.edu.br>
2022-06-07 16:23:12 -0300

    integração blog

--- Index/script.js
+++ Index/script.js
@@ -9,6 +9,7 @@ api = 'http://127.0.0.1:3061'
 $(document).ready(() => {
     users.list();
     blog.postagem();
+    blogposts.postagens();
 });
 
 
@@ -42,33 +43,26 @@ var user = {
 
     insert() {
         var nome = document.querySelector("#nome").value;
-        // var dataN = document.querySelector("#dataN").value;
-        // var cpf = parseInt(document.querySelector("#cpf").value);
-        // var rg = parseInt(document.querySelector("#rg").value);
-        // var ds_locais = document.querySelector("#ds_locais").value;
+        var dataN = document.querySelector("#dataN").value;
+        var cpf = parseInt(document.querySelector("#cpf").value);
+        var rg = parseInt(document.querySelector("#rg").value);
+        var ds_locais = document.querySelector("#ds_locais").value;
         var apl = document.querySelector("#apelido").value;
-        // var locaisOndeFica = document.querySelector("#locais_e").value;
-        // var alb_abr = document.querySelector("#al_ab").value;
-        // var domiciP = document.querySelector("#domP").value;
-        // var qt_ruaS = document.querySelector("#qt_ruaSem").value;
-        // var qt_alb = document.querySelector("#qt_albSem").value;
-        // var qt_domPart = document.querySelector("#qt_domP").value;
-        // var tempR = document.querySelector("tempoRua").value;
-        // var motivoR = document.querySelector("#motivoRua").value;
-        // var qt_Cid = document.querySelector("#qtCidade").value;
-        // var FamRua = document.querySelector("#famVRua").value;
-        // var atvCom7 = document.querySelector("#atvComutarias7dias").value;
-        // var UltLugares6 = document.querySelector("#lugares6M").value;
-        // var cTrabalho = document.querySelector("#cartTrab").value;
-        // var fazDinheiro = document.querySelector("#oqFazDinheiro").value;
-        // var pRenda = document.querySelector("#progRenda").value;
-        // var qPRenda = document.querySelector("#qProgRenda").value;
+        var locaisOndeFica = document.querySelector("#locais_e").value;
+        var alb_abr = document.querySelector("#al_ab").value;
+        var domiciP = document.querySelector("#domP").value;
+        var qt_ruaS = document.querySelector("#qt_ruaSem").value;
+        var qt_alb = document.querySelector("#qt_albSem").value;
+        var qt_domPart = document.querySelector("#qt_domP").value;
+
+    
+
         if (nome) {
             if (nome.trim() != '') {
                 $.ajax({
                     type: 'POST',
                     url: api + '/userinsert',
-                    data: {nm_nome: nome, apelido: apl},
+                    data: {nm_nome: nome, dt_nas: dataN, nr_cpf: cpf, nr_rg: rg, ds_locais: ds_locais},
                 }).done(function () {
                     users.list();
                 }).fail(function (msg) {
@@ -95,6 +89,7 @@ var blog = {
                         tx += '<h2 class="ttl2">' + element.tlt_post + '</h2>';
                         tx += '<p class="txt1">' + element.dt_post + '</p>';
                         tx += '<p class="txt1">' + element.txt_post + '</p>';
+                        
                         tx += '<div class="actions">';
                         tx += '</div>';
                     tx += '</div>';
@@ -106,4 +101,58 @@ var blog = {
     }
     
 };
+
+var blogposts = {
+    
+    postagens() {
+        $.ajax({
+            url: api + '/vePostagem',
+            type: 'GET',
+            success: data => {
+                var tx = '';
+                tx += '<div class="centerList">';
+                data.forEach(element => {
+                    tx += '<div class="card slim">';
+                        tx += '<h2 class="ttl2">' + element.tlt_post + '</h2>';
+                        tx += '<p class="txt1">' + element.dt_post + '</p>';
+                        tx += '<div class="actions">';
+                        tx += '</div>';
+                    tx += '</div>';
+                });
+                $('#postsblog').html(tx);
+            }
+        });
+        
+    }
+    
+};
+
+var subirPost = { //Colocar no onClick
+
+    insert() {
+        var titulo_post = document.querySelector("#titulo_post").value;
+        var dataP = document.querySelector("#dataP").value;
+        var img = document.querySelector("#img_post").value;
+        var texto_post = document.querySelector("#texto_post").value;
+        
+
+    
+
+        if (titulo_post) {
+            if (titulo_post.trim() != '') {
+                $.ajax({
+                    type: 'POST',
+                    url: api + '/criaPostagem',
+                    data: {tlt_post: titulo_post ,  dt_post : dataP, img : img, txt_post : texto_post},
+                }).done(function () {
+                    blog.postagem();
+                }).fail(function (msg) {
+                    //console.log('FAIL');
+                }).always(function (msg) {
+                    //console.log('ALWAYS');
+                });
+            }
+        }
+    },
+}
 
