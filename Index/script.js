237241125838ec370672456483fd5cc67fe634b7
rodrigorodriguesb <rodrigo.rodrigues@sou.inteli.edu.br>
2022-06-07 16:13:50 -0300

    integraçaõ(post)

--- Index/script.js
+++ Index/script.js
@@ -42,35 +42,33 @@ var user = {
 
     insert() {
         var nome = document.querySelector("#nome").value;
-        var dataN = document.querySelector("#dataN").value;
-        var cpf = parseInt(document.querySelector("#cpf").value);
-        var rg = parseInt(document.querySelector("#rg").value);
-        var ds_locais = document.querySelector("#ds_locais").value;
+        // var dataN = document.querySelector("#dataN").value;
+        // var cpf = parseInt(document.querySelector("#cpf").value);
+        // var rg = parseInt(document.querySelector("#rg").value);
+        // var ds_locais = document.querySelector("#ds_locais").value;
         var apl = document.querySelector("#apelido").value;
-        var locaisOndeFica = document.querySelector("#locais_e").value;
-        var alb_abr = document.querySelector("#al_ab").value;
-        var domiciP = document.querySelector("#domP").value;
-        var qt_ruaS = document.querySelector("#qt_ruaSem").value;
-        var qt_alb = document.querySelector("#qt_albSem").value;
-        var qt_domPart = document.querySelector("#qt_domP").value;
-        var tempR = document.querySelector("tempoRua").value;
-        var motivoR = document.querySelector("#motivoRua").value;
-        var qt_Cid = document.querySelector("#qtCidade").value;
-        var FamRua = document.querySelector("#famVRua").value;
-        var atvCom7 = document.querySelector("#atvComutarias7dias").value;
-        var UltLugares6 = document.querySelector("#lugares6M").value;
-        var cTrabalho = document.querySelector("#cartTrab").value;
-        var fazDinheiro = document.querySelector("#oqFazDinheiro").value;
-        var pRenda = document.querySelector("#progRenda").value;
-
-    
-
+        // var locaisOndeFica = document.querySelector("#locais_e").value;
+        // var alb_abr = document.querySelector("#al_ab").value;
+        // var domiciP = document.querySelector("#domP").value;
+        // var qt_ruaS = document.querySelector("#qt_ruaSem").value;
+        // var qt_alb = document.querySelector("#qt_albSem").value;
+        // var qt_domPart = document.querySelector("#qt_domP").value;
+        // var tempR = document.querySelector("tempoRua").value;
+        // var motivoR = document.querySelector("#motivoRua").value;
+        // var qt_Cid = document.querySelector("#qtCidade").value;
+        // var FamRua = document.querySelector("#famVRua").value;
+        // var atvCom7 = document.querySelector("#atvComutarias7dias").value;
+        // var UltLugares6 = document.querySelector("#lugares6M").value;
+        // var cTrabalho = document.querySelector("#cartTrab").value;
+        // var fazDinheiro = document.querySelector("#oqFazDinheiro").value;
+        // var pRenda = document.querySelector("#progRenda").value;
+        // var qPRenda = document.querySelector("#qProgRenda").value;
         if (nome) {
             if (nome.trim() != '') {
                 $.ajax({
                     type: 'POST',
                     url: api + '/userinsert',
-                    data: {nm_nome: nome, dt_nas: dataN, nr_cpf: cpf, nr_rg: rg, ds_locais: ds_locais},
+                    data: {nm_nome: nome, apelido: apl},
                 }).done(function () {
                     users.list();
                 }).fail(function (msg) {
